b26962197d198c9bcbd5c1af0c5a06425d383db3
EB1811 <44376246+EB1811@users.noreply.github.com>
2020-07-24 14:07:17 +0100

    Create Quiz page CSS

--- src/Components/Quiz/CreateQuiz.js
+++ src/Components/Quiz/CreateQuiz.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { createQuiz } from '../../Store/Actions/quizActions';
 import Question from './Question';
 import { Redirect } from 'react-router-dom';
+import Nav from '../Layout/Nav';
+import { NavLink } from 'react-router-dom';
 
 class CreateQuiz extends Component {
 
@@ -10,7 +12,8 @@ class CreateQuiz extends Component {
         title: '',
         questions: [],
         currentQuestion: '',
-        currentAnswer: ''
+        currentAnswer: '',
+        currentStage: 0
     }
 
     // Create question / answer button function.
@@ -24,6 +27,14 @@ class CreateQuiz extends Component {
 
             this.setState({ questions: [...this.state.questions, newQuestion]});
             //console.log(this.state)
+        }
+    }
+
+    nextStage = () => {
+        if(this.state.currentStage < 2) {
+            this.setState({ 
+                currentStage: this.state.currentStage + 1
+            });
         }
     }
 
@@ -51,37 +62,95 @@ class CreateQuiz extends Component {
         if(authStatus.isLoaded) {
         // Route guarding
         if(!authStatus.uid) return <Redirect to='/login' />
-        
-        return (
-            <div className="container">
-                <form onSubmit={this.handleSubmit} className="white">
-                    <h5 className="grey-text text-darken-3">Create Quiz</h5>
-                    <div className="input-field">
-                        <label htmlFor="title">Title</label>
-                        <input required type="text" id="title" onChange={this.handleChange}/>
-                    </div>
+            if(this.state.currentStage === 0) {
+                return (
+                    <div>
+                        <Nav/>
+                        <NavLink to='/'>
+                            <div className="backButton">
+                                <a class="btn-floating btn-large waves-effect hoverable waves-light deep-purple">
+                                    <i class="material-icons">arrow_back</i>
+                                </a>
+                            </div>
+                        </NavLink>
+                        <div className="valign-wrapper">
+                            <h1 className="title">Title</h1>
 
-                    <div className="row of">
-                        <div className="col s6 offset-s2">
-                            <Question questions={this.state.questions}/>
-                            <div className="input-field">
-                                <label htmlFor="question">Qustion</label>
-                                <input type="text" id="currentQuestion" onChange={this.handleChange}/>
+                            <div className="container">
+                                <div>
+                                <div className="row">
+                                    <div className="col s6 offset-s3">
+                                        <div className="input-field">
+                                            <input required type="text" id="title" onChange={this.handleChange}/>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="row">
+                                    <div className="col s12 center">
+                                        <button type="button" onClick={this.nextStage} className="btn-large hoverable deep-purple">Next</button>
+                                    </div>
+                                </div>
+                                </div>
                             </div>
-                            <div className="input-field">
-                                <label htmlFor="answer">Answer</label>
-                                <input type="text" id="currentAnswer" onChange={this.handleChange}/>
-                            </div>
-                            <button type="button" onClick={this.addQuestion} className="btn pink lighten-1">Add Question</button>
                         </div>
                     </div>
+                )
+            } else if(this.state.currentStage === 1) {
+                return (
+                    <div>
+                        <Nav/>
+                        <NavLink to='/'>
+                            <div className="backButton">
+                                <a class="btn-floating btn-large waves-effect hoverable waves-light deep-purple">
+                                    <i class="material-icons">arrow_back</i>
+                                </a>
+                            </div>
+                        </NavLink>
+                        <div className="valign-wrapper">
+                            <div className="container">
+                                <form onSubmit={this.handleSubmit} className="">
+                                    <div className="row">
+                                        <div className="col s12 center">
+                                            <h1 className="createQuizTitle">Questions</h1>
+                                        </div>
+                                    </div>
+                                    <div className="row areaMargin">
+                                        <div className="row noBottomMargin">
+                                            <div className="col s6 offset-s3">
+                                                <Question questions={this.state.questions}/>
+                                            </div>
+                                        </div>
+                                        <div className="row noBottomMargin">
+                                            <div className="col s5 offset-s1">
+                                                <div className="input-field">
+                                                    <label htmlFor="question">Qustion</label>
+                                                    <input type="text" id="currentQuestion" onChange={this.handleChange}/>
+                                                </div>
+                                            </div>
+                                            <div className="col s5">
+                                                <div className="input-field">
+                                                    <label htmlFor="answer">Answer</label>
+                                                    <input type="text" id="currentAnswer" onChange={this.handleChange}/>
+                                                </div>
+                                            </div>
+                                            <div className="col s6 offset-s3 center">
+                                                <button type="button" onClick={this.addQuestion} className="btn-large btn2 hoverable deep-purple">Add Question</button>
+                                            </div>
+                                        </div>
+                                    </div>
 
-                    <div className="input-field">
-                        <button className="btn pink lighten-1">Create Quiz</button>
+                                    <div className="row">
+                                        <div className="input-field center">
+                                            <button className="btn-large hoverable deep-purple">Create Quiz</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-                </form>
-            </div>
-        )
+                )
+            }
         } else {
             return null;
         }
--- src/Components/Quiz/Question.js
+++ src/Components/Quiz/Question.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Question = ({questions}) => {
     return (
         <ul className="collection with-header">
-            <li className="collection-header"><h6>Current Questions</h6></li>
+            <li className="collection-header deep-purple"><h5 className="colTitle">Current Questions</h5></li>
             { questions && questions.map(question =>{
                 return (
-                    <div className="collection-item" key={question.id}>
+                    <div className="collection-item deep-purple colText" key={question.id}>
                         Question: {question.question} <br/>
                         Answer: {question.answer}
                     </div>
