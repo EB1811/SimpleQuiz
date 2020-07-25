7b7e2577e9ae67796e5a7ce2f668f50d2ab2f0ae
EB1811 <44376246+EB1811@users.noreply.github.com>
2020-07-25 18:26:14 +0100

    Small css changes

--- src/Components/Quiz/QuizCard.js
+++ src/Components/Quiz/QuizCard.js
@@ -7,6 +7,11 @@ const QuizCard = ({quiz}) => {
             <div className="card deep-purple hoverable project-summary">
                 <div className="card-content">
                     <h5 className="quizTitle">{quiz.title}</h5>
+                    
+                    { quiz.question.length > 1 
+                    ? <p className="secondHeader">{quiz.question.length} questions</p> 
+                    : <p className="secondHeader">{quiz.question.length} question</p>  }
+
                     <p className="infoText grey-text">Posted by {quiz.author}</p>
                     <p className="infoText grey-text">{  moment(quiz.dateCreated.toDate()).calendar() }</p>
                 </div>
